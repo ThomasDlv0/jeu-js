2ef197d7b1b99bc1121d71fe7a2bb88f15910623
ThomasDlv0 <thomas.delville000@icloud.com>
2025-10-15 11:26:01 +0200

    ajout variable et object

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Variables globales
+let playerScore = 0;
+let pcScore = 0;
+const MAX_SCORE = 10;
+
+// Emojis pour l'affichage
+const powerEmojis = {
+    feu: '🔥',
+    eau: '💧',
+    terre: '🌍'
+};
